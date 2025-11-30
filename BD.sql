a1cd172503998629090ae348a77d3ee660dc718b
AlexisHal8 <alexis756gg@gmail.com>
2025-11-29 22:10:26 -0600

    Primer commit

--- BD.sql
+++ BD.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS paquexpress_db;
 USE paquexpress_db;
 
--- Tabla de Usuarios (Agentes)
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -10,18 +9,18 @@ CREATE TABLE users (
     is_active BOOLEAN DEFAULT TRUE
 );
 
--- Tabla de Paquetes
+
 CREATE TABLE packages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tracking_number VARCHAR(50) UNIQUE NOT NULL,
     destination_address TEXT NOT NULL,
-    -- Coordenadas del destino (para el mapa del agente)
+
     dest_lat FLOAT NOT NULL,
     dest_lng FLOAT NOT NULL,
     status ENUM('pendiente', 'entregado') DEFAULT 'pendiente',
     assigned_agent_id INT,
     
-    -- Campos de evidencia de entrega
+
     proof_photo_url VARCHAR(255),
     delivery_lat FLOAT,
     delivery_lng FLOAT,
@@ -30,6 +29,4 @@ CREATE TABLE packages (
     FOREIGN KEY (assigned_agent_id) REFERENCES users(id)
 );
 
--- Insertar un agente de prueba (Password: '123456')
--- Nota: En producción el password se inserta ya hasheado, aquí es solo ejemplo
--- El hash real lo generaremos con la API más adelante.
+
